a90d85186d665e8c111b639c87f9de4f0ffa8f8b
DPoojasni <141192714+DPoojasni@users.noreply.github.com>
2023-08-29 22:11:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Netflix-like Homepage</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="netflix-logo.png" alt="Netflix Logo">
+        </nav>
+    </header>
+
+    <section class="hero">
+        <div class="hero-content">
+            <h1>Unlimited Movies, TV Shows, and More.</h1>
+            <p>Watch anywhere. Cancel anytime.</p>
+            <a href="#" class="btn btn-primary">Watch Now</a>
+        </div>
+    </section>
+
+    <section class="featured">
+        <h2>Featured Content</h2>
+        <div class="featured-content">
+            <div class="featured-card">
+                <img src="movie-1.jpg" alt="Movie 1">
+            </div>
+            <div class="featured-card">
+                <img src="movie-2.jpg" alt="Movie 2">
+            </div>
+            <div class="featured-card">
+                <img src="movie-3.jpg" alt="Movie 3">
+            </div>
+            <!-- Add more featured cards as needed -->
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Netflix-like Homepage</p>
+    </footer>
+</body>
+</html>
